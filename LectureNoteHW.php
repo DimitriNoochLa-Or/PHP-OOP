9293cec88e101cd1bd5605eedb6e24c4cb6b970a
Dimitri Nooch La-Or <dimitrinoochlaor@gmail.com>
2015-01-06 18:42:02 -0800

    third one

--- LectureNoteHW.php
+++ LectureNoteHW.php
@@ -1,9 +1,9 @@
 
 <?php 
-class Fish{
+class Tiger{
 	public $firstName = "default name";
 	public $lastName = "default last name";
-	public $type = "sushi";
+	public $type = "animal";
 	public $price = 0;
 
 		function getName() {
@@ -11,8 +11,8 @@ class Fish{
 			"{$this->lastName}";
 		}
 }
-	$fish1 = new Fish();
-	$fish1->firstName = "bob";
-	$fish1->lastName = "sushi";
-	print "the fish's name is {$fish1->getName()}.";
+	$tiger1 = new Tiger();
+	$tiger1->firstName = "MR.micman ";
+	$tiger1->lastName = "liger";
+	print "the tigers's name is {$tiger1->getName()}.";
 ?>

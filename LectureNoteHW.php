1b54ce44609c3787fd762f43e2d7e92e628cf19b
Dimitri Nooch La-Or <dimitrinoochlaor@gmail.com>
2015-01-07 21:31:00 -0800

    lecture notes

--- LectureNoteHW.php
+++ LectureNoteHW.php
@@ -1,18 +1,54 @@
 
 <?php 
-class Tiger{
-	public $firstName = "default name";
-	public $lastName = "default last name";
-	public $type = "animal";
-	public $price = 0;
-
+// Example 1
+class Cat {
+public $firstName;
+public $lastName;
+public $breed;
+	function __construct($title, $firstName, $lastName, $breed) {
+	$this­>firstName = $firstName;
+	$this­>lastName = $lastName;
+	$this­>breed = $breed;
+	}
 		function getName() {
-			return "{$this->firstName}" . 
-			"{$this->lastName}";
+		return "{$this­>firstName}" .
+		"{$this­>lastName}";
 		}
 }
-	$tiger1 = new Tiger();
-	$tiger1->firstName = "MR.micman ";
-	$tiger1->lastName = "liger";
-	print "the tigers's name is {$tiger1->getName()}.";
-?>
+class Tiger {
+public $firstName;
+public $lastName;
+public $breed;
+	function __construct($title, $firstName, $lastName, $breed) {
+	$this­>firstName = $firstName;
+	$this­>lastName = $lastName;
+	$this­>breed = $breed;
+	}
+		function getName() {
+		return "{$this­>firstName}" .
+		"{$this­>lastName}";
+		}
+}
+class Icecream {
+public $shape;
+public $flavor;
+public $place;
+	function __construct($title, $shape, $flavor, $place) {
+	$this­>shape = $shape;
+	$this­>flavor = $flavor;
+	$this­>place = $place;
+	}
+		function getName() {
+		return "{$this­>shape}" .
+		"{$this­>flavor}";
+		}
+}
+
+ $cat1 = new Cat("billybob", "sherwood", "a cat");
+ print "Cat 1:{$cat1->getName()}\n";
+
+ $tiger1 = new Tiger("tiger", "stripes", "a cat");
+ print "Tiger 1:{$tiger1->getName()}\n";
+
+ $icecream1 = new Icecream("square", "chocolate", "baskin robins");
+ print "Icecream 1:{$icecream1->getName()}\n";

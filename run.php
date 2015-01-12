9fe76216b02d99754de467fce3ec417b7fc1d6c2
Dimitri Nooch La-Or <dimitrinoochlaor@gmail.com>
2015-01-12 13:31:03 -0800

    for the lecture today

--- run.php
+++ run.php
@@ -1,30 +1,52 @@
 <?php  
-if (is_integer('908098')) echo "this is a number";
-else 
-echo "this is not a number";
-var_dump(is_integer(213048));
-var_dump(is_integer(1234));
-var_dump(is_integer(123));
-var_dump(is_integer(231));
+// if (is_integer('908098')) echo "this is a number";
+// else 
+// echo "this is not a number";
+// var_dump(is_integer(213048));
+// var_dump(is_integer(1234));
+// var_dump(is_integer(123));
+// var_dump(is_integer(231));
 
-// example 2
-$a = false;
+// // example 2
+// $a = false;
 
-// Since $a is a boolean, it will return true
-if (is_bool($a) === true) {
-    echo "Yes, this is a boolean";
-}
+// // Since $a is a boolean, it will return true
+// if (is_bool($a) === true) {
+//     echo "Yes, this is a boolean";
+// }
 
-// Since $b is not a boolean, it will return false
-else  {
-    echo "No, this is not a boolean";
-}
+// // Since $b is not a boolean, it will return false
+// else  {
+//     echo "No, this is not a boolean";
+// }
 
-// example 3 
-if (is_double('908098')) echo "this is a number";
-else 
-echo "this is not a number";
-var_dump(is_double(213048));
-var_dump(is_double(1234));
-var_dump(is_double(123));
-var_dump(is_double(231));
+// // example 3 
+// if (is_double('908098')) echo "this is a number";
+// else 
+// echo "this is not a number";
+// var_dump(is_double(213048));
+// var_dump(is_double(1234));
+// var_dump(is_double(123));
+// var_dump(is_double(231));
+ class car
+ {
+  public $models;
+  public $colors;
+
+ 	function __construct($models,$colors)
+ 	{
+	 	$this->models = $models;
+	 	$this->colors = $colors;
+	 }
+    function welcome()
+    {
+  		return "welcome to the dealership, we many " .
+  		 $this->models . " in many " . $this->colors;
+    }
+
+ }
+  
+
+  $car = new Car(' honda civics', ' colors' );
+
+  print $car->welcome();
